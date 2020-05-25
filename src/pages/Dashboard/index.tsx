36093966f4031decfaee74f2ed4f28bb8efa9b5e
Leandro Martins <leandro.martins.85@gmail.com>
2020-05-24 21:10:00 -0300

    Estilização do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/40571653?s=460&u=8d3dd470a1946ccb9d10da12044b1f9ea0d67554&v=4"
+            alt="Leandro Martins"
+          />
+          <div>
+            <strong>Teste</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/40571653?s=460&u=8d3dd470a1946ccb9d10da12044b1f9ea0d67554&v=4"
+            alt="Leandro Martins"
+          />
+          <div>
+            <strong>Teste</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/40571653?s=460&u=8d3dd470a1946ccb9d10da12044b1f9ea0d67554&v=4"
+            alt="Leandro Martins"
+          />
+          <div>
+            <strong>Teste</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
